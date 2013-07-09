e140f8e0ad446c67fcc2bb4495007e4c5a9dd816
djamelz <djamelz@gmail.com>
2013-07-10 00:52:02 +0200

    recursive & iterative B&B DFS - Python

---
+++ 2-Knapsack/Tests/test_solver.py
@@ -0,0 +1,38 @@
+__author__ = 'Djamel'
+
+import unittest
+import heapq
+import solver
+
+
+class TestSolver(unittest.TestCase):
+
+    def test_BranchAndBound(self):
+        # lines = [(-8, solver.Item(8, 4)), (-10, solver.Item(10, 5)), (-15, solver.Item(15, 8)), (-4, solver.Item(4, 3))]
+        # heapq.heapify(lines)
+        lines = [solver.Item(15, 8, 2),  solver.Item(10, 5, 1), solver.Item(8, 4, 0), solver.Item(4, 3, 3)]
+        self.assertEqual(solver.BranchAndBound(lines, 11, 37), (19, [0, 0, 1, 1]))
+
+    def test_dynamic_programming(self):
+        lines = [solver.Item(8, 4), solver.Item(10, 5), solver.Item(15, 8), solver.Item(4, 3)]
+        self.assertEqual(solver.dynamicProgramming(lines, 11), (19, [0, 0, 1, 1]))
+
+    def test_create_list(self):
+        input_data = open("//projects/Coursera-DiscreteOptimization/2-Knapsack/data/ks_4_0").read(-1)
+        self.assertEqual(solver.create_list(input_data), ([solver.Item(8, 4), solver.Item(10, 5), solver.Item(15, 8), solver.Item(4, 3)], 11))
+
+    def test_solveIt(self):
+        input_data = open("//projects/Coursera-DiscreteOptimization/2-Knapsack/data/ks_4_0").read(-1)
+        self.assertEqual(solver.solveIt(input_data), "19 0\n0 0 1 1")
+
+    def test_solveIt2(self):
+        input_data = open("//projects/Coursera-DiscreteOptimization/2-Knapsack/data/ks_45_0").read(-1)
+        self.assertEqual(solver.solveIt(input_data), "23974 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0")
+
+    def test_solveIt3(self):
+        input_data = open("//projects/Coursera-DiscreteOptimization/2-Knapsack/data/ks_200_0").read(-1)
+        self.assertEqual(solver.solveIt(input_data), "100062 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0")
+
+    def test_estimator(self):
+        lines = [solver.Item(8, 4), solver.Item(10, 5), solver.Item(15, 8), solver.Item(4, 3)]
+        self.assertEqual(solver.estimator(lines, 0, 11), 21.75)
---
+++ 2-Knapsack/solver.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+from numpy import zeros
+from copy import deepcopy
+
+class Item:
+    def __init__(self, value, weight, position=0, score=0):
+        self.value = value
+        self.weight = weight
+        self.position = position
+        self.score = 0
+        self.chocolate = float(self.value)/float(self.weight)
+
+    def add_value(self, value):
+        self.value += value
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class BbItem:
+
+    def __init__(self, value, maxEstimate, list=None):
+        self.value = value
+        self.maxEstimate = maxEstimate
+        if list == None:
+            self.list = []
+        else:
+            self.list = list
+
+    def add_value(self, value):
+        return BbItem(self.value + value, self.maxEstimate, self.list)
+
+
+    # def withItem(self):
+    #     return BbItem(self.value, self.score, self.list + [1])
+
+    def withoutItem(self):
+        return BbItem(self.value, self.maxEstimate, self.list + [0])
+
+    def addToList(self, position):
+        self.list.append(position)
+
+    def withItem(self, position):
+        return BbItem(self.value, self.maxEstimate, self.list + [position])
+
+    def orderedList(self, count):
+        xx = zeros(count, int)
+        xx[self.list] = 1
+        return xx.tolist()
+
+
+
+
+def solveIt(inputData):
+    t0 = time.time()
+    # Modify this code to run your optimization algorithm
+
+    #value, taken = withDynamicProgramming(inputData)
+    # value, taken = withBranchAndBound(inputData)
+    value, taken = withIterativeBranchAndBound(inputData)
+
+    # prepare the solution in the specified output format
+    outputData = str(value) + ' ' + str(0) + '\n'
+    outputData += ' '.join(map(str, taken))
+
+    print time.time() - t0, "seconds process time"
+
+    return outputData
+
+def withDynamicProgramming(inputData):
+    heap, capacity = create_list(inputData)
+    value, taken = dynamicProgramming(heap, capacity)
+    return value, taken
+
+def withBranchAndBound(inputData):
+    list, capacity, estimate =  create_sorted_list(inputData)
+    return BranchAndBound(list, capacity, estimate)
+
+def withIterativeBranchAndBound(inputData):
+    list, capacity, estimate =  create_sorted_list(inputData)
+    return IterativeBranchAndBound(list, capacity, estimate)
+
+
+def create_list(inputData):
+    # parse the input
+    lines = inputData.split('\n')
+
+    capacity = int(lines[0].split()[1])
+
+    items = []
+    maxValue = 0
+
+    for i in range(1, len(lines) - 1):
+        line = lines[i].split()
+
+        item = Item(int(line[0]), int(line[1]))
+        maxValue += item.value
+        items.append(item)
+
+    return items, capacity
+
+
+def create_sorted_list(inputData):
+    # parse the input
+    lines = inputData.split('\n')
+
+    capacity = int(lines[0].split()[1])
+
+    items = []
+
+    for i in range(1, len(lines) - 1):
+        if len(lines[i].strip()) > 0:
+            line = lines[i].split()
+
+            item = Item(int(line[0]), int(line[1]), i - 1)
+            items.append(item)
+
+    estimate = estimator(items, 0, capacity)
+    items.sort(key=lambda item: item.chocolate, reverse=True)
+    return items, capacity, estimate
+
+def dynamicProgramming(items, capacity, list=[]):
+    if len(items) == 0:
+        return 0, list
+
+    withoutItem = dynamicProgramming(items[1:], capacity, list + [0])
+    if items[0].weight <= capacity:
+        withItem = dynamicProgramming(items[1:], capacity - items[0].weight,  list + [1])
+        withItem = withItem[0] + items[0].value, withItem[1]
+        if withItem[0] > withoutItem[0]:
+            return withItem
+    return withoutItem
+
+maxScore = BbItem(0, 0)
+
+def BranchAndBound(items, capacity, estimate):
+    print "capacity =", capacity, "Estimate =", estimate
+    BranchAndBoundAcc(items, len(items), 0, capacity, BbItem(0, estimate), estimate)
+    return maxScore.value, maxScore.orderedList(len(items))
+
+
+def BranchAndBoundAcc(items, size, it, capacity, bbItem, maxEstimate):
+    global maxScore
+
+    if size == it or capacity == 0 or maxEstimate < maxScore.value:
+        if bbItem.value > maxScore.value:
+            maxScore = bbItem
+        return
+    item = items[it]
+
+    if item.weight <= capacity:
+        BranchAndBoundAcc(items, size, it + 1, capacity - item.weight, bbItem.add_value(item.value).withItem(item.position), maxEstimate)
+
+    estimate = estimator(items, it + 1, capacity)
+    BranchAndBoundAcc(items, size, it + 1, capacity, bbItem, estimate + bbItem.value)
+
+    return
+
+
+def IterativeBranchAndBound(items, capacity, estimate):
+    maxItem = BbItem(0, 0)
+
+    stack = []
+    endList = len(items) - 1
+    currentCapacity = capacity
+
+    stack.append((0, False, capacity, BbItem(0, estimate)))
+    stack.append((0, True, capacity, BbItem(0, estimate)))
+
+    while(len(stack) > 0):
+        it, withItem, currentCapacity, currentItem  = stack.pop()
+
+        if withItem and items[it].weight <= currentCapacity:
+            currentCapacity -= items[it].weight
+            currentItem.value += items[it].value
+            currentItem.addToList(items[it].position)
+        else:
+            currentItem.score = estimator(items, it+1, currentCapacity) + currentItem.value
+
+        if currentCapacity == 0 or currentItem.maxEstimate < maxItem.value or it == endList:
+            if currentItem.value > maxItem.value:
+                maxItem = currentItem
+            continue
+
+        if it < endList:
+            stack.append((it + 1, False, deepcopy(currentCapacity), deepcopy(currentItem)))
+            stack.append((it + 1, True, deepcopy(currentCapacity), deepcopy(currentItem)))
+
+    return maxItem.value, maxItem.orderedList(len(items))
+
+
+def estimator(items, it, capacity):
+    estimate = 0
+    size = len(items)
+    for i in xrange(it, size):
+        if items[i].weight >= capacity:
+            estimate += capacity * items[i].chocolate
+            break
+        else:
+            estimate += items[i].value
+            capacity = capacity - items[i].weight
+    return estimate
+
+
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        fileLocation = sys.argv[1].strip()
+        inputDataFile = open(fileLocation, 'r')
+        inputData = ''.join(inputDataFile.readlines())
+        inputDataFile.close()
+        print solveIt(inputData)
+    else:
+        print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)'
+
